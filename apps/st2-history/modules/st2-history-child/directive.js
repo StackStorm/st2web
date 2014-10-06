cbd232ac95e03fb162a1aa48b2cf549044dbd60c
Kirill Izotov <enykeev@stackstorm.com>
2014-10-06 14:47:53 +0700

    Replace tables with flex boxes for list view
    It would give us more flexibility when styling multiple tables to look the same. It would also help to contain the complexity.

--- apps/st2-history/modules/st2-history-child/directive.js
+++ apps/st2-history/modules/st2-history-child/directive.js
@@ -1,14 +1,14 @@
 'use strict';
 
 angular.module('main')
-  .directive('st2TableChild', function () {
+  .directive('st2HistoryChild', function () {
 
     return {
       restrict: 'C',
       scope: {
         'children': '='
       },
-      templateUrl: 'apps/st2-history/modules/st2-table-child/template.html',
+      templateUrl: 'apps/st2-history/modules/st2-history-child/template.html',
       link: function postLink() {}
     };
 
