619012c8581b19d1076ecd8100f8b985c7878763
Kirill Izotov <enykeev@stackstorm.com>
2014-10-09 13:11:06 +0700

    Add an ability to pick server in UI

--- modules/st2-menu/directive.js
+++ modules/st2-menu/directive.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('main')
-  .directive('st2Menu', function () {
+  .directive('st2Menu', function ($window) {
 
     return {
       restrict: 'C',
@@ -14,6 +14,33 @@ angular.module('main')
 
         scope.isActive = function (e) {
           return scope.state.includes(e);
+        };
+
+        scope.getServer = function () {
+          var url = localStorage.getItem('st2Host');
+          return url ? _.findKey(scope.serverList, function (e) {
+            return e === url;
+          }) : 'Mock API';
+        };
+
+        scope.setServer = function (server) {
+          var url = scope.serverList[server];
+          if (url) {
+            localStorage.setItem('st2Host', url);
+          } else {
+            localStorage.removeItem('st2Host');
+          }
+
+          $window.location.reload();
+        };
+
+        scope.serverList = {
+          'Stage 1': '//st2stage001.stackstorm.net:9101',
+          'Stage 2': '//st2stage002.stackstorm.net:9101',
+          'Stage 3': '//st2stage003.stackstorm.net:9101',
+          'Stage 4': '//st2stage004.stackstorm.net:9101',
+          'Dev Env': '//172.168.50.50:9101',
+          'Mock API': null
         };
       }
     };
