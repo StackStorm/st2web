1801eebfb184361ee637285ae20b5ee26d43cae6
Kirill Izotov <enykeev@stackstorm.com>
2014-10-27 15:53:40 +0600

    Allow collapsing list groups

---
+++ modules/st2-flex-table/directive.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('main')
+  .directive('st2FlexTable', function ($rootScope) {
+
+    return {
+      restrict: 'C',
+      transclude: true,
+      template: '<div ng-transclude></div>',
+      link: function postLink(scope, element) {
+        scope.toggle = function () {
+          element.toggleClass('st2-flex-table--contracted');
+        }
+      }
+    };
+
+  });
