325502b9f7331a65b0269af7e62c2580e36da947
Kirill Izotov <enykeev@stackstorm.com>
2014-10-27 16:09:45 +0600

    Show filters on click instead of hover

--- modules/st2-filter/directive.js
+++ modules/st2-filter/directive.js
@@ -10,19 +10,24 @@ angular.module('main')
         label: '@'
       },
       templateUrl: 'modules/st2-filter/template.html',
-      link: function postLink(scope) {
+      link: function postLink(scope, element) {
         scope._api = st2Api;
 
         scope.$watch('_api.historyFilters.list() | unwrap', function (filters) {
           scope.filters = filters;
         });
 
-        scope.toggle = function (name) {
+        scope.toggle = function () {
+          element.toggleClass('st2-filter--active');
+        };
+
+        scope.pick = function (name) {
           var o = _.clone(scope.$root.state.params);
 
           o[scope.type] = o[scope.type] === name ? void 0 : name;
 
           scope.$root.state.go('.', o);
+          scope.toggle();
         };
       }
     };
