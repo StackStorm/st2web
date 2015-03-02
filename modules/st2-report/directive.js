0e2d5888dc7e203a424f2bc45606304ee32ae3a4
Kirill Izotov <enykeev@stackstorm.com>
2015-03-02 10:41:24 +0600

    Add "Report a bug" button on every page

---
+++ modules/st2-report/directive.js
@@ -0,0 +1,11 @@
+'use strict';
+
+angular.module('main')
+  .directive('st2Report', function () {
+
+    return {
+      restrict: 'C',
+      templateUrl: 'modules/st2-report/template.html'
+    };
+
+  });
