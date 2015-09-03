ee092f150981c6bac7c592164784f70ee944e56d
Edward Medvedev <edward.medvedev@gmail.com>
2015-09-03 16:48:16 +0600

    Redesigning rules to convey the "if/then" flow

--- apps/st2-rules/controller.js
+++ apps/st2-rules/controller.js
@@ -137,13 +137,11 @@ angular.module('main')
           };
 
           st2api.client.packs.list().then(function (packs) {
+            $scope.icons = {};
             _(packs).forEach(function(pack) {
-              if (pack.name in $scope.groups && pack.files.indexOf('icon.png') >= 0) {
+              if (pack.files && pack.files.indexOf('icon.png') >= 0) {
                 var icon_path = st2api.client.packFile.route(pack.name+'/icon.png');
-                $scope.groups[pack.name]['icon'] = icon_path;
-                if ($scope.ruleMeta.pack && $scope.ruleMeta.pack === pack.name) {
-                  $scope.iconPath = icon_path;
-                }
+                $scope.icons[pack.name] = icon_path;
               }
             });
             $scope.$apply();
