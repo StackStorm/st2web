10815682cb89c2f642a4130401754a6867de8e24
Dmitri Zimin(e) <dz@stackstorm.com>
2016-02-03 15:27:38 -0800

    Manually move prod defaults from PR278
    To make packages work on 1.3.1 - https://github.com/StackStorm/st2web/pull/278

--- config.js
+++ config.js
@@ -1,14 +1,17 @@
 'use strict';
 angular.module('main')
   .constant('st2Config', {
-
-    hosts: [{
-      name: 'Dev Env',
-      url: '//172.168.50.50:9101'
-    }, {
-      name: 'Express',
-      url: '//172.168.90.50:9101',
-      auth: true
-    }]
-
+  // Use defaults for reference single-box deployment.
+  // Use examples below for advanced setup.
+  // hosts: [{
+  //    // With nginx as reverse proxy
+  //    name: 'StackStorm',
+  //    url: 'https://:443/api',
+  //    auth: 'https://:443/auth'
+  //  }, {
+  //    // Dev env settings with auth off
+  //    name: 'Dev Env',
+  //    url: '//172.168.90.50:9101',
+  //    auth: false
+  // }]
   });
