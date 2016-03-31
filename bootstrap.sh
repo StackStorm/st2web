d9130788320347c54036e7a953afbc0d0f0f1083
Kirill Enykeev <enykeev@gmail.com>
2016-03-31 13:12:30 +0600

    Save exact version numbers for dependencies

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,14 @@ apt-get -y update
 
 # Install N node version manager
 apt-get -y install bash git curl make
-curl -L https://git.io/n-install | su vagrant -c "bash -s -- -y lts"
+curl -L https://git.io/n-install | su vagrant -c "bash -s -- -q lts"
+. /home/vagrant/.bashrc
+
+# Configure npm
+cat <<EOF > /home/vagrant/.npmrc
+loglevel=http
+save-prefix=
+EOF
 
 # Change default CWD to /vagrant
 echo "cd /vagrant" >> /home/vagrant/.bashrc
