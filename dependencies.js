cb819e4b4f976abb1122f22cb59ecca2ca535db0
Kirill Enykeev <enykeev@gmail.com>
2016-03-31 17:20:59 +0600

    Remove angular-moment and angular-sanitize dependencies
    We're not using them for an embarrassingly long time

--- dependencies.js
+++ dependencies.js
@@ -2,7 +2,5 @@
 
 module.exports = angular.module('main.dependencies', [
   require('angular-ui-router'),
-  require('angular-moment') && 'angularMoment',
-  require('angular-sanitize'),
   require('angular-ui-notification')
 ]);
