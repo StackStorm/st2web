ab76230ddd8e24e0eb26916c1f54afb9c77069c5
Kirill Enykeev <enykeev@gmail.com>
2016-04-05 13:46:32 +0600

    Rewrite st2-label in react as an experiment

---
+++ modules/st2-label/label.component.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var React = require('react');
+
+var states = {
+  'complete': {
+    class: 'st2-label--success'
+  },
+  'error': {
+    class: 'st2-label--danger'
+  },
+  'enabled': {
+    class: 'st2-label--success'
+  },
+  'disabled': {
+    class: 'st2-label--danger'
+  },
+  'succeeded': {
+    class: 'st2-label--succeeded'
+  },
+  'failed': {
+    class: 'st2-label--failed'
+  },
+  'running': {
+    class: 'st2-label--progress'
+  },
+  'scheduled': {
+    class: 'st2-label--progress'
+  }
+};
+
+function capitalize(string) {
+  if (!string || !string.charAt) {
+    return string;
+  }
+  return string.charAt(0).toUpperCase() + string.slice(1).toLowerCase();
+}
+
+class Label extends React.Component {
+  static propTypes = {
+    status : React.PropTypes.string
+  }
+  render() {
+    var props = {
+      className: 'st2-label__label'
+    };
+
+    var state = states[this.props.status];
+
+    if (state) {
+      props.className += ' ' + states[this.props.status].class;
+    }
+
+    return <span {...props}>
+      { capitalize(state && state.title || this.props.status) }
+    </span>;
+  }
+}
+
+module.exports = Label;
--- tests/util.js
+++ tests/util.js
@@ -6,6 +6,10 @@ var zombie = require('zombie');
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
 module.exports = function (browser) {
+  browser.on('opened', function (win) {
+    win.__REACT_DEVTOOLS_GLOBAL_HOOK__ = {};
+  });
+
   browser.pipeline.addHandler(function(browser, request) {
     var url = new URI(request.url);
 
