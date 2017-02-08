c82f42e5b62800ffa3973852d8315e3e4324ef11
Edward Medvedev <edward.medvedev@gmail.com>
2017-02-08 18:35:43 +0100

    Add extensions

--- apps/index.js
+++ apps/index.js
@@ -1,9 +1,12 @@
 'use strict';
 
-module.exports = angular.module('main.apps', [
+var apps = [
   require('./st2-docs').name,
   require('./st2-actions').name,
   require('./st2-history').name,
   require('./st2-login').name,
   require('./st2-rules').name
-]);
+];
+var extensions = require('./extensions');
+
+module.exports = angular.module('main.apps', apps.concat(extensions));
