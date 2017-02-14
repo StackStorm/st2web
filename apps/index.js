8b4c8e64e8ae936aa693dfe19431b6666a873f9f
Edward Medvedev <edward.medvedev@gmail.com>
2017-02-14 11:54:27 +0100

    Bring back index.js, remove extensions

--- apps/index.js
+++ apps/index.js
@@ -1,12 +1,9 @@
 'use strict';
 
-var apps = [
+module.exports = angular.module('main.apps', [
   require('./st2-docs').name,
   require('./st2-actions').name,
   require('./st2-history').name,
   require('./st2-login').name,
   require('./st2-rules').name
-];
-var extensions = require('./extensions');
-
-module.exports = angular.module('main.apps', apps.concat(extensions));
+]);
