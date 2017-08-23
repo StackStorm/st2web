f94b0bd16482b9e1c2677e00e41057a8385b9cd2
Kirill Izotov <enykeev@stackstorm.com>
2017-08-23 15:12:35 +0700

    Remove st2-report
    We don't need it anymore since we're now opening up Reamaze with a menu button.
    
    # Conflicts:
    #	apps/st2-actions/template.html
    #	apps/st2-rules/template.html

--- modules/st2-report/index.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-var mod = module.exports = angular.module('main.modules.st2Report', []);
-
-var directive = require('./report.directive.js');
-
-mod
-  .directive(directive.name, directive)
-  ;
