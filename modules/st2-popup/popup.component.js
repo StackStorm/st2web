8ace9e02e6520e2692a855ebf85b1d3a6d6e60f6
Christopher Baker <christopher@bitovi.com>
2017-12-02 12:14:24 -0800

    st2-popup

--- modules/st2-popup/popup.component.js
+++ modules/st2-popup/popup.component.js
@@ -3,7 +3,33 @@ import { PropTypes } from 'prop-types';
 
 import './style.less';
 
-export default class Panel extends React.Component {
+export class Popup extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    onCancel: PropTypes.func,
+    children: PropTypes.node,
+    className: PropTypes.string,
+  }
+
+  render() {
+    const { title, onCancel, children, className, ...props } = this.props;
+
+    return (
+      <div {...props} className={`st2-popup ${className || ''}`} onClick={onCancel}>
+        <div className="st2-details st2-panel__details st2-popup__details" onClick={(e) => e.stopPropagation()}>
+          <div className="st2-panel__scroller">
+            { title ? (
+              <PopupTitle>{title}</PopupTitle>
+            ) : null }
+            { children }
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export class PopupTitle extends React.Component {
   static propTypes = {
     children: PropTypes.node,
     className: PropTypes.string,
@@ -13,9 +39,11 @@ export default class Panel extends React.Component {
     const { children, className, ...props } = this.props;
 
     return (
-      <div className={`st2-popup ${className}`} {...props}>
+      <div {...props} className={`st2-popup__title ${className || ''}`}>
         { children }
       </div>
     );
   }
 }
+
+export default Popup;
