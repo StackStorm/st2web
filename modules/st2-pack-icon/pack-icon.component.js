f602488590e58d4fe68bca0beafdb48077d6e949
Christopher Baker <christopher@bitovi.com>
2018-01-03 14:35:57 -0800

    st2-pack-icon: fetch from server

--- modules/st2-pack-icon/pack-icon.component.js
+++ modules/st2-pack-icon/pack-icon.component.js
@@ -2,8 +2,11 @@ import React from 'react';
 import { PropTypes } from 'prop-types';
 import cx from 'classnames';
 import api from '@stackstorm/module-api';
+import notification from '@stackstorm/module-notification';
 
 import './style.less';
+const icons = {};
+let iconPromise = null;
 
 export default class PackIcon extends React.Component {
   static propTypes = {
@@ -16,14 +19,39 @@ export default class PackIcon extends React.Component {
     small: false,
   }
 
+  componentWillMount() {
+    if (Object.keys(icons).length > 0) {
+      return;
+    }
+
+    if (iconPromise) {
+      iconPromise.then(() => {
+        this.forceUpdate();
+      });
+
+      return;
+    }
+
+    iconPromise = api.client.packs.list().then((packs) => {
+      packs.map(({ ref, files }) => {
+        if (files && files.indexOf('icon.png') >= 0) {
+          icons[ref] = api.client.packFile.route(`${ref}/icon.png`);
+        }
+      });
+    }).catch((res) => {
+      notification.error('Unable to retrieve pack icons. See details in developer tools console.');
+      console.error(res); // eslint-disable-line no-console
+      throw res;
+    });
+  }
+
   render() {
     const { className, name, small, ...props } = this.props;
-    const src = name && api.client.packFile.route(`${name}/icon.png`);
 
     return (
       <span {...props} className={cx('st2-pack-icon', className, { 'st2-pack-icon-small': small })}>
-        { src ? (
-          <img className={cx('st2-pack-icon__image', { 'st2-pack-icon__image-small' : small })} src={src} />
+        { icons[name] ? (
+          <img className={cx('st2-pack-icon__image', { 'st2-pack-icon__image-small' : small })} src={icons[name]} />
         ) : null }
       </span>
     );
