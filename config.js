e7836a5858c64e304f74060aabe20c42fc251c34
Christopher Baker <christopher@bitovi.com>
2018-01-09 11:19:19 -0800

    restore angular config

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 'use strict';
 
-window.st2constants = window.st2constants || {};
-window.st2constants.st2Config = {
+/* global angular */
+angular.module('main')
+  .constant('st2Config', {
 
-  // hosts: [
-  //   {
-  //     name: 'Dev Env',
-  //     url: '//172.168.50.50:9101/api',
-  //     auth: '//172.168.50.50:9101/auth',
-  //   },
-  //   {
-  //     name: 'Express',
-  //     url: '//172.168.90.50:9101/api',
-  //     auth: '//172.168.90.50:9101/auth',
-  //   },
-  // ],
+    // hosts: [
+    //   {
+    //     name: 'Dev Env',
+    //     url: '//172.168.50.50:9101/api',
+    //     auth: '//172.168.50.50:9101/auth',
+    //   },
+    //   {
+    //     name: 'Express',
+    //     url: '//172.168.90.50:9101/api',
+    //     auth: '//172.168.90.50:9101/auth',
+    //   },
+    // ],
 
-};
+  });
