07fc91af0e48625aa8cf7809b4988b7369d7dfc9
Tomaz Muraus <tomaz@tomaz.me>
2018-09-20 17:20:34 +0200

    Read version directly from package.json file.

--- tasks/production/package-metadata.js
+++ tasks/production/package-metadata.js
@@ -8,13 +8,15 @@ const fs = require('fs');
 const path = require('path');
 
 gulp.task('production-package-metadata', (done) => {
-  var result, git_sha, data, file_path;
-  result = child_process.spawnSync('git', ['rev-parse', '--short', 'HEAD']);
+  var result, git_sha, data, file_path, pkg_version;
 
+  result = child_process.spawnSync('git', ['rev-parse', '--short', 'HEAD']);
   git_sha = result.stdout.toString().trim();
 
+  pkg_version = require(path.resolve('./package.json')).st2_version;
+
   data = '[st2web]\n'
-  data += 'version = ' + process.env.PKG_VERSION + '\n';
+  data += 'version = ' + pkg_version + '\n';
   data += 'git_sha = ' + git_sha + '\n';
   data += 'circle_build_url = ' + process.env.ST2_CIRCLE_URL;
 
