abc4f33facf0007ba3cc3fb7bf645ac07f4b8c7f
Tomaz Muraus <tomaz@tomaz.me>
2018-09-24 13:20:54 +0200

    Fix lint.

--- tasks/production/package-metadata.js
+++ tasks/production/package-metadata.js
@@ -1,26 +1,21 @@
 'use strict';
 
 const gulp = require('gulp');
-const settings = require('../settings.json');
-const plugins = require('gulp-load-plugins')(settings.plugins);
 const child_process = require('child_process');
 const fs = require('fs');
 const path = require('path');
 
 gulp.task('production-package-metadata', (done) => {
-  var result, git_sha, data, file_path, pkg_version;
+  const circle_build_url = process.env.CIRCLE_BUILD_URL;
 
-  result = child_process.spawnSync('git', ['rev-parse', '--short', 'HEAD']);
-  git_sha = result.stdout.toString().trim();
+  const result = child_process.spawnSync('git', [ 'rev-parse', '--short', 'HEAD' ]);
+  const git_sha = result.stdout.toString().trim();
 
-  pkg_version = require(path.resolve('./package.json')).st2_version;
+  const pkg_version = require(path.resolve('./package.json')).st2_version;
 
-  data = '[st2web]\n'
-  data += 'version = ' + pkg_version + '\n';
-  data += 'git_sha = ' + git_sha + '\n';
-  data += 'circle_build_url = ' + process.env.CIRCLE_BUILD_URL;
+  const data = `[st2web]\nversion = ${pkg_version}\ngit_sha = ${git_sha}\ncircle_build_url = ${circle_build_url}\n`;
 
-  file_path = path.join(path.resolve('./build'), 'package.meta');
+  const file_path = path.join(path.resolve('./build'), 'package.meta');
 
   fs.writeFileSync(file_path, data);
   done();
