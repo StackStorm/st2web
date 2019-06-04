993a30c51f8d52a1dab47643f810d1fecaafcbe3
armab <dev@arma.biz>
2019-06-04 13:41:08 +0100

    Fix st2web to work on HTTP
    Don't redirect HTTP -> HTTPS on app level if user initially requested HTTP

--- modules/st2-api/api.js
+++ modules/st2-api/api.js
@@ -65,9 +65,9 @@ export class API {
     }
     else {
       this.server = {
-        api: `https://${window.location.host}/api`,
-        auth: `https://${window.location.host}/auth`,
-        stream: `https://${window.location.host}/stream`,
+        api: `${window.location.protocol}//${window.location.host}/api`,
+        auth: `${window.location.protocol}//${window.location.host}/auth`,
+        stream: `${window.location.protocol}//${window.location.host}/stream`,
         token: !_.isEmpty(token) ? token : null,
       };
     }
