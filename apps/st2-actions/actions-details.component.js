63b6a6f47f4eb7e52dc39234277b9c38f3dc310c
pradnya-orchestral <81607514+pradnya-orchestral@users.noreply.github.com>
2021-12-02 12:27:48 +0530

    Regression issue fix for delete action checkbox

--- apps/st2-actions/actions-details.component.js
+++ apps/st2-actions/actions-details.component.js
@@ -87,7 +87,7 @@ export default class ActionsDetails extends React.Component {
     destinationAction:'',
     packs:[],
     isRemoveFiles : false,
-
+    
   }
 
   componentDidMount() {
@@ -158,8 +158,9 @@ export default class ActionsDetails extends React.Component {
   }
 
   componentDidUpdate(prevProps, prevState) {
-    const { id, filter } = this.props;
+    const { id , filter} = this.props;
 
+ 
     if (id && id !== prevProps.id) {
       this.fetchAction(id);
     }
@@ -391,7 +392,7 @@ export default class ActionsDetails extends React.Component {
       this.setState({isChecked:false});
     }
   }
-  
+
   handleDeleteChange(e) {
     const isChecked = document.getElementById('deletecheckbox').checked;
     if(isChecked) {
@@ -419,7 +420,6 @@ export default class ActionsDetails extends React.Component {
   closeDeleteModel() {
     document.getElementById('delete').style.display = 'none';
   }
-  
 
   render() {
     const { section, action, executions, entrypoint} = this.props;
@@ -464,7 +464,7 @@ export default class ActionsDetails extends React.Component {
               <Button flat value="Preview" onClick={() => this.handleToggleRunPreview()} />
               <DetailsToolbarSeparator />
               <Button disabled={this.props.id !== action.ref} className="st2-forms__button st2-details__toolbar-button"  value="Clone" onClick={(e) => this.openModel(e)}   />
-              <Button className="st2-forms__button st2-details__toolbar-button"  value="Delete"  onClick={(e) => this.openDeleteModel(e)}  />
+              <Button className="st2-forms__button st2-details__toolbar-button"  value="Delete" onClick={(e) => this.openDeleteModel(e)}  />
 
               { action.runner_type === 'mistral-v2' || action.runner_type === 'orquesta' ? (
                 <Link
@@ -580,11 +580,10 @@ export default class ActionsDetails extends React.Component {
           </div>
         </div>
 
-
         <div id="delete" className="web_dialog_overlay" style={{display: 'none', position: 'fixed', zIndex: '10', left: '0',top: '0',width: '100%', minHeight: '-webkit-fill-available', overflow: 'auto', backgroundColor: 'rgba(0,0,0,0.4)' }}> 
           <div id="dialog" className="web_dialog" style={{backgroundColor: '#fefefe' ,margin: '15% auto',padding: '20px', border: '1px solid #888' ,width: '24%' ,height:'20%' }}>
             <p> You are about to delete the action. Are you sure? </p>
-            <input id="deletecheckbox" name="checkbox" type="checkbox" checked={this.state.isRemoveFiles} value={this.state.isRemoveFiles} onChange={(e) => this.handleChange(e)}  /> Remove Files (This operation is irreversible.) <br /><br /><br />
+            <input id="deletecheckbox" name="checkbox" type="checkbox" checked={this.state.isRemoveFiles} value={this.state.isRemoveFiles} onChange={(e) => this.handleDeleteChange(e)} /> Remove Files (This operation is irreversible.) <br /><br /><br />
             <div style={{width:'100%', display:'inline-block'}}>
               <button  onClick={(e) => this.handleDelete(e)}  type="submit" className="btn" style={{backgroundColor: '#04AA6D' , color: 'white',padding: '16px 20px' ,border: 'none', cursor: 'pointer', width: '45%' ,marginBottom:'10px' , opacity: '0.8',float:'left'}}>Submit</button>
               <button onClick={(e) => this.closeDeleteModel(e)} type="close" className="btn cancel" style={{backgroundColor: 'red' , color: 'white',padding: '16px 20px' ,border: 'none', cursor: 'pointer', width: '45%' ,marginBottom:'10px' , opacity: '0.8', float:'right'}}>Close</button>
